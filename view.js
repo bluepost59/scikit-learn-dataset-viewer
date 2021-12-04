69722f8278acb112599b33f06f3d82d4531a9e57
bluepost59 <kaput.kat@gmail.com>
2021-12-04 16:43:45 +0900

    テストプロット

--- view.js
+++ view.js
@@ -1,6 +1,7 @@
 console.log("view.js");
 
 window.onload = async () => {
+    test_plot_data();
 
     const pyodide = await loadPyodide({
         indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
@@ -10,4 +11,17 @@ window.onload = async () => {
     await pyodide.loadPackage("scikit-learn");
 
     console.log("pyodide is ready");
+}
+
+// まずばbreast_cancer_dataset
+async function test_plot_data() {
+    const xx = [1, 2, 3, 4, 5];
+    const yy = [2, 10, 15, 10, 21];
+
+    Plotly.newPlot("graph_area", [{
+        x: xx,
+        y: yy,
+        mode: "markers",
+        type: "scatter",
+    }]);
 }
